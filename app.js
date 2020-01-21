6196c090a8fb8d5242eb9a7d9750d6fcdba4fc5b
hsuifang <ifang_hsu@hpicorp.com.tw>
2020-01-21 23:28:32 +0800

    test

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let sendButton = document.querySelector('button');
 function send() {
   let name = document.querySelector('#nameValue').value;
   let phone = document.querySelector('#phoneValue').value;
-
+  let url = 'https://spreadsheets.google.com/feeds/cells/1ppewigMFQe3UdxTR4RYwHl-sX3s9b66jnGfykdRz9dQ/1/public/values?alt=json'
   $.ajax({
     type: "get",
     url: "https://script.google.com/macros/s/AKfycbz9gJNch20Usl0J9Gyl344c2trrIJAKtyItK00jtUlHCTNmjYI/exec",
